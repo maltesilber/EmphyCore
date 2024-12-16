b8d1c8f6831829d92c839cad9d957ce690a7ac57
MaltchineLearning <78181358+MaltchineLearning@users.noreply.github.com>
2024-12-16 11:47:04 +0100

    initial commit

--- laa.py
+++ laa.py
@@ -33,7 +33,7 @@ def compute_laa(img, lung_mask, t=-950):
     if img.shape != lung_mask.shape:
         raise ValueError("CT scan and lung mask must have the same dimensions.")
     laa = img[lung_mask > 0] < t
-    return np.sum(laa) / np.sum(lung_mask > 0)*100, (laa*1).astype(np.uint8)*255
+    return np.sum(laa) / np.sum(lung_mask > 0)*100, laa.astype(np.uint8)
 
 
 def categorize_laa(laa_value):
